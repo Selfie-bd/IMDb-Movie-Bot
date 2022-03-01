b7e7c7ad0f6515ca0e979c1e967b26d54c9fdab9
Selfie SD <webtamil0@gmail.com>
2022-03-02 05:19:21 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -182,4 +182,4 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
             logger.exception(e)
             await msg.edit(f'Error: {e}')
         else:
-            await msg.edit(f'Succesfully saved <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>')
+            await msg.edit(f'🔥Succesfully saved <code>{total_files}</code> to dataBase!\n❗Duplicate Files Skipped: <code>{duplicate}</code>\n⚠️Deleted Messages Skipped: <code>{deleted}</code>\n❌Non-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\n🤧Errors Occurred: <code>{errors}</code>')
